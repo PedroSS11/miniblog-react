0f16afa6600c4f44e1fd54a1ab656f162d1006fd
Pedro Henrique <contatophss11@gmail.com>
2022-09-20 15:29:09 -0300

    validação post

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -74,6 +74,35 @@ export const useAuthentication = () => {
         signOut(auth)
     }
 
+    //LOGIN - SIGN IN
+    const login = async(data) => {
+
+        checkIfIsCancelled()
+
+        setLoading(true)
+        setError(false)
+
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password)
+            setLoading(false)
+        } catch (error) {
+            let systemErrorMessage
+
+            if(error.message.includes("user-not-found")) {
+                systemErrorMessage = 'Usuário não encontrado'
+            } else if (error.message.includes("wrong-password")) {
+                systemErrorMessage = 'Senha incorreta'
+            } else {
+                systemErrorMessage = 'Ocorreu um erro, por favor tente mais tarde'
+            }
+
+            setError(systemErrorMessage)
+            setLoading(false)
+            
+        }
+
+    }
+
     useEffect(() => {
         return () => setCancelled(true)
     }, [])
@@ -83,7 +112,8 @@ export const useAuthentication = () => {
         createUser,
         error,
         loading,
-        logout
+        logout,
+        login
     }
 
 };
--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,9 +1,98 @@
 import styles from './CreatePost.module.css'
 
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuthValue } from '../../context/AuthContext'
+import { useInsertDocument } from '../../hooks/useInsertDocument'
+
 const CreatePost = () => {
+
+  const [title, setTitle]  = useState("")
+  const [image, setImage] = useState("")
+  const [body, setBody] = useState("")
+  const [tags, setTags] = useState([])
+  const [formError, setFormError] = useState("")
+
+  const {insertDocument, response} = useInsertDocument("posts")
+
+  const {user} = useAuthValue()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setFormError("")
+
+    // validar url da imagem 
+
+    //criar o array de tags
+
+    // checar todos os valores
+
+    insertDocument({
+      title,
+      image, 
+      body,
+      tags,
+      uid: user.uid,
+      createdBy: user.displayName
+    })
+
+    // redirect Home page
+
+  }
+
   return (
-    <div>
-        <h1>Create Post</h1>
+    <div className={styles.create__post}>
+        <h2>Criar Post</h2>
+        <p>Escreva sobre o que quiser e compatilhe o seu conhecimento !</p>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Título: </span>
+            <input type="text" 
+            name='title' 
+            required 
+            placeholder='Pense num bom título...' 
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            />
+          </label>
+
+          <label>
+            <span>URL da imagem: </span>
+            <input type="text" 
+            name='image' 
+            required 
+            placeholder='Insira uma imagem que representa o seu post' 
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+            />
+          </label>
+
+          <label>
+            <span>Conteúdo: </span>
+            <textarea name="body"
+            required 
+            placeholder='insira o conteudo do post'
+            onChange={(e) => setBody(e.target.value)}
+            value={body}></textarea>
+          </label>
+
+          <label>
+            <span>Tags: </span>
+            <input type="text" 
+            name='tags' 
+            required 
+            placeholder='Insira as tags separadas por vígula' 
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+            />
+          </label>
+          
+          {!response.loading && <button className='btn'>Cadastrar</button>}
+          {response.loading && (
+            <button className='btn' disabled>Aguarde...</button>
+          )}
+          {response.error && <p className='error'>{response.error}</p>}
+        </form>
     </div>
   )
 }
