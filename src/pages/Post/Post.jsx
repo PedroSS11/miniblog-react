baceb010f259167823ff3a57946c4cd35121b164
Pedro Henrique <contatophss11@gmail.com>
2022-09-26 11:35:48 -0300

    done v1

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -4,7 +4,8 @@ import styles from './Post.module.css'
 import { useParams } from 'react-router-dom'
 import { useFetchDocument } from '../../hooks/useFetchDocument'
 
-
+// ROUTER
+import { Link } from 'react-router-dom'
 const Post = () => {
 
   const { id } = useParams()
@@ -18,13 +19,14 @@ const Post = () => {
         <>
           <h1>{post.title}</h1>
           <img src={post.image} alt={post.title} />
-          <p>{post.body}</p>
+          <p><span>Conteúdo:</span> {post.body}</p>
           <h3>Este post trata sobre:</h3>
           <div className={styles.tags}>
             {post.tagsArray.map((tag) => (
               <p key={tag}><span>#</span>{tag}</p>
             ))}
           </div>
+          <Link to='/' className='btn'>Voltar</Link>
         </>
       )}
     </div>
